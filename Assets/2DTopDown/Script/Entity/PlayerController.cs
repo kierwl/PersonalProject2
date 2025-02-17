f4c02886e15221fa5081d4b665111d2ad157a282
kierwl <45115106+kierwl@users.noreply.github.com>
2025-02-17 12:26:52 +0900

    feat: 그라운드 타일 생성
    타일맵 생성

--- Assets/2DTopDown/Script/Entity/PlayerController.cs
+++ Assets/2DTopDown/Script/Entity/PlayerController.cs
@@ -2,17 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+namespace Topdown
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerController : BaseController
     {
-        
-    }
+        private Camera camera;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        protected override void Start()
+        {
+            base.Start();
+            camera = Camera.main;
+        }
+
+        protected override void HandleAction()
+        {
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+            movementDirection = new Vector2(horizontal, vertical).normalized;
+
+            Vector2 mousePosition = Input.mousePosition;
+            Vector2 worldPos = camera.ScreenToWorldPoint(mousePosition); // 해상도 좌표를 월드 좌표로 변환
+            lookDirection = (worldPos - (Vector2)transform.position);
+
+            if (lookDirection.magnitude < .9f)
+            {
+                lookDirection = Vector2.zero;
+            }
+            else
+            {
+                lookDirection = lookDirection.normalized;
+            }
+        }
     }
 }
