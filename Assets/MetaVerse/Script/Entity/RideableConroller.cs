fb176ed07d853b25c1ebd7e3dca57cc0ebdb07ab
kierwl <45115106+kierwl@users.noreply.github.com>
2025-02-20 09:53:52 +0900

    Fix 상호작용 개선
    탑승물이 상호작용 스크립트 없이도 작동되게끔 수정

--- Assets/MetaVerse/Script/Entity/RideableConroller.cs
+++ Assets/MetaVerse/Script/Entity/RideableConroller.cs
@@ -7,6 +7,12 @@ public class RideableController : BaseEntity
 
     protected override void Update()
     {
+        if (Input.GetKeyDown(KeyCode.F) && isRiding)// 탑승여부 관계없이 f를 눌러 내릴 수 있음
+        {
+            ExitRide();
+            return;
+        }
+
         if (isRiding)
         {
             base.Update(); // 탑승 중일 때만 이동 가능
@@ -16,5 +22,10 @@ public class RideableController : BaseEntity
     public void SetRiding(bool riding)
     {
         isRiding = riding;
+    }
+    private void ExitRide()
+    {
+        // Rideable 스크립트에서 탈출 기능을 실행하도록 설정
+        GetComponent<Rideable>().Interact();
     }
 }
