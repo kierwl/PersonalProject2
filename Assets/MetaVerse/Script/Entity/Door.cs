6905d8ca8de3d62a9ead24592f1294e8d107a5a9
kierwl <45115106+kierwl@users.noreply.github.com>
2025-02-20 12:15:56 +0900

    Fix: 기능수정
    문을열면 콜라이더 비활성화
    
    씬 이동 수정

--- Assets/MetaVerse/Script/Entity/Door.cs
+++ Assets/MetaVerse/Script/Entity/Door.cs
@@ -7,16 +7,19 @@ public class Door : MonoBehaviour, IInteractable
 {
     private Animator animator;
     private bool isOpen = false;
+    private BoxCollider2D boxCollider;
 
     void Start()
     {
         animator = GetComponentInChildren<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>(); // BoxCollider2D 가져오기
     }
 
     public void Interact()
     {
         isOpen = !isOpen;
         animator.SetBool("isOpen", isOpen);
+        boxCollider.enabled = !isOpen; // 문이 열리면 콜라이더 비활성화, 닫히면 활성화
         Debug.Log(isOpen ? "문이 열렸습니다!" : "문이 닫혔습니다!");
     }
 }
